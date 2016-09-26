ac37541f43fec4a37de57703568dfb62f82ba00c
lance <lance@weicitech.com>
2016-09-26 14:43:29 +0800

    修改stringutils的字符串长度

--- src/main/java/com/jeeframework/util/string/StringUtils.java
+++ src/main/java/com/jeeframework/util/string/StringUtils.java
@@ -1543,7 +1543,49 @@ public abstract class StringUtils extends com.jeeframework.util.string.apache.St
     }
 
 
-    public static String getRandomString(int length) { //length表示生成字符串的长度
+    /**
+     * 得到指定长度字符串，只包含数字
+     *
+     * @param length
+     * @return
+     */
+    public static String getRandomNumberString(int length) { //length表示生成字符串的长度
+        //        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        String base = "0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 得到指定长度字符串，只包含数字，不包含0
+     *
+     * @param length
+     * @return
+     */
+    public static String getRandomNumberStringNoZero(int length) { //length表示生成字符串的长度
+        //        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        String base = "123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 得到指定长度字符串，只包含数字英文
+     *
+     * @param length
+     * @return
+     */
+    public static String getRandomAlphaNumericString(int length) { //length表示生成字符串的长度
         //        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
         String base = "0123456789";
         Random random = new Random();
